25ef74b560ae6d7f9afd48187c267813d3ff2699
sbroadhurst <stepbro31@gmail.com>
2022-05-31 12:56:49 -0400

    fix for tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Dashboard</title>
         <meta name="description" content="Dashboard example" />
         <link rel="icon" href="/favicon.ico" />
 
-        <div>dashboard home page</div>
+        <div>
+          <p>dashboard home page</p>
+        </div>
       </Head>
     </div>
   )
