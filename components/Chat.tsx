d7185ac196c4677bf94290528b7da5f0c92dd181
sbroadhurst <stepbro31@gmail.com>
2022-06-05 14:54:58 -0400

    navbar items

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
+
+import Button from './Button'
+import { chatData } from '../public/data/dummy'
+import { useStateContext } from '../contexts/ContextProvider'
+import Image from 'next/image'
 
 const Chat = () => {
-  return <div>Chat</div>
+  const { currentColor } = useStateContext()
+
+  return (
+    <div className="nav-item absolute right-5 top-16 w-96 rounded-lg bg-slate-100 p-8 dark:bg-[#42464D] md:right-52">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-3">
+          <p className="text-lg font-semibold dark:text-gray-200">Messages</p>
+          <button type="button" className="bg-orange  rounded p-1 px-2 text-xs text-white">
+            5 New
+          </button>
+        </div>
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
+      </div>
+      <div className="mt-5 ">
+        {chatData?.map((item: any, index: number) => (
+          <div key={index} className="border-b-1 border-color flex cursor-pointer items-center gap-5 p-3 leading-8">
+            <div className="relative">
+              <Image height={'40px'} width={'40px'} className="rounded-full" src={item.image} alt={item.message} />
+              <span
+                style={{ background: currentColor }}
+                className="absolute right-0 -top-1 inline-flex h-2 w-2 rounded-full"
+              />
+            </div>
+            <div>
+              <p className="font-semibold dark:text-gray-200 ">{item.message}</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">{item.desc}</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">{item.time}</p>
+            </div>
+          </div>
+        ))}
+        <div className="mt-5">
+          <Button color="white" bgColor={currentColor} text="See all messages" borderRadius="10px" width="full" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Chat
--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
+
+import Button from './Button'
+import { userProfileData } from '../public/data/dummy'
+import { useStateContext } from '../contexts/ContextProvider'
+import avatar from '../public/data/avatar.jpg'
+import Image from 'next/image'
 
 const UserProfile = () => {
-  return <div>UserProfile</div>
+  const { currentColor } = useStateContext()
+
+  return (
+    <div className="nav-item absolute right-1 top-16 w-96 rounded-lg bg-slate-100 p-8 dark:bg-[#42464D]">
+      <div className="flex items-center justify-between">
+        <p className="text-lg font-semibold dark:text-gray-200">User Profile</p>
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
+      </div>
+      <div className="border-color border-b-1 mt-6 flex items-center gap-5 pb-6">
+        <Image height={'96px'} width={'96px'} className="rounded-full" src={avatar} alt="user-profile" />
+        <div>
+          <p className="text-xl font-semibold dark:text-gray-200"> Michael Roberts </p>
+          <p className="text-sm text-gray-500 dark:text-gray-400"> Administrator </p>
+          <p className="text-sm font-semibold text-gray-500 dark:text-gray-400"> info@shop.com </p>
+        </div>
+      </div>
+      <div>
+        {userProfileData.map((item, index) => (
+          <div
+            key={index}
+            className="border-b-1 border-color flex cursor-pointer gap-5 p-4 hover:bg-light-gray  dark:hover:bg-[#42464D]">
+            <button
+              type="button"
+              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              className=" rounded-lg p-3 text-xl hover:bg-light-gray">
+              {item.icon}
+            </button>
+
+            <div>
+              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400"> {item.desc} </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="mt-5">
+        <Button color="white" bgColor={currentColor} text="Logout" borderRadius="10px" width="full" />
+      </div>
+    </div>
+  )
 }
 
 export default UserProfile
